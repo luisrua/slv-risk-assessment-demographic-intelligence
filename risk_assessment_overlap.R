d630b8dd83fa504cb688c0b484cceacf953e9200
luisrua <luisruarodriguez@gmail.com>
2025-12-18 17:18:05 +0000

    table in excell with clean format and first bar charts

--- risk_assessment_overlap.R
+++ risk_assessment_overlap.R
@@ -30,7 +30,7 @@ mutate(across(where(is.character), function(x) {
   # Try to repair common Spanish encoding issues (Latin1 -> UTF-8)
   fixed_text <- iconv(x, from = "latin1", to = "UTF-8")
   # If iconv fails (returns NA), keep the original text but strip bad bytes
-  ifelse(is.na(fixed_text), iconv(x, to = "UTF-8", sub = ""), fixed_text)
+  # ifelse(is.na(fixed_text), iconv(x, to = "UTF-8", sub = ""), fixed_text)
 }))
 
 # Population input - use Worldpop 2025 for the moment
@@ -117,7 +117,7 @@ risk_dist_wide <- risk_dist %>%
   pivot_wider(
     names_from = risk_class,
     values_from = wpop_risk,
-    values_fill = 0) %>% 
+    values_fill = 0) %>%
   select(c(NA3, Extremo, Alto, Medio, Bajo)) %>% 
   mutate(wpop_risk = Extremo + Alto + Medio + Bajo, # to calculate total population per district
          per_flood_extremo = Extremo / wpop_risk,
@@ -153,19 +153,6 @@ risk_layer_tile <- haz_flood_pop %>%
 #             layer ='floods_tile',
 #             overwrite=T)
 
-# Export table in excel
-# Clean the data encoding
-
-# risk_layer_clean <- risk_layer_dist %>%
-#   st_drop_geometry() %>%
-#   as.data.frame() %>%
-#   mutate(across(where(is.character), function(x) {
-#     # Try to repair common Spanish encoding issues (Latin1 -> UTF-8)
-#     fixed_text <- iconv(x, from = "latin1", to = "UTF-8")
-#     # If iconv fails (returns NA), keep the original text but strip bad bytes
-#     ifelse(is.na(fixed_text), iconv(x, to = "UTF-8", sub = ""), fixed_text)
-#   }))
-
 write.xlsx(as.data.frame(risk_layer_tile),
            file = paste0(dir,"tables/slv_risk_assessment_districts.xlsx"),
            sheetName = "flood_risk",
@@ -179,7 +166,7 @@ write.xlsx(as.data.frame(risk_layer_tile),
 # Importing lac admin boundaries 
 ab <- vect(paste0(dir,"layers/lac_ab_pol_4326.gpkg"), layer = "lac_ab_pol_4326" )
 ab <-  st_make_valid(st_as_sf(ab))
-plot(ab)
+# plot(ab)
 
 # Testing custom palette
 # This palette moves from Light Grey (Low-Low) to Blue (High Flood) and Orange (High Vuln),
@@ -198,11 +185,11 @@ custom_pal_red <- c(
 
 # --- 1. Prepare Data & Palette (Same as before) ---
 risk_layer_biv <- risk_layer_dist %>% 
-  mutate(flood_exp_ext_alt = per_flood_extremo + per_flood_alto,
-         IVMC = replace_na(IVMC, 0))
+  mutate(flood_exp_ext_alt = per_flood_extremo + per_flood_alto)
 
 # Add a tiny random number to the problematic columns
 risk_layer_biv <- risk_layer_biv %>%
+  filter(!is.na(IVMC) & !is.na(flood_exp_ext_alt)) %>% # Remove NAs to create the legend
   mutate(
     # factor = 0.001 adds microscopic noise to separate identical values
     IVMC_jitter = jitter(IVMC, factor = 0.001), 
@@ -218,8 +205,8 @@ data_bivariate <- bi_class(st_as_sf(risk_layer_biv),
 # --- 2. Create High-Res Legend (Same as before) ---
 legend <- bi_legend(pal = custom_pal_red,
                     dim = 3,
-                    xlab = "Higher Flood Exposure",
-                    ylab = "Higher Vulnerability",
+                    xlab = "Mayor Amenaza Inundaciones",
+                    ylab = "Mayor Vulnerabilidad",
                     size = 12) + # Base size
   theme(
     axis.title.x = element_text(size = 18), # Customize X label
@@ -241,7 +228,8 @@ ab <- ab %>%
 
 # Layers for high risk districts
 high_risk_labels <- data_bivariate %>% 
-  filter(bi_class == "3-3")
+  filter(bi_class %in% c("3-3","2-3","3-2"))
+
 # Increase bbox to fit labels at the edges of the map# 1. Create a slightly larger bounding box (e.g., 5% bigger)
 bbox_new <- st_bbox(data_bivariate) # Get original box
 
@@ -254,6 +242,10 @@ bbox_new["xmin"] <- bbox_new["xmin"] - (0.05 * x_range)
 bbox_new["xmax"] <- bbox_new["xmax"] + (0.05 * x_range)
 bbox_new["ymin"] <- bbox_new["ymin"] - (0.05 * y_range)
 bbox_new["ymax"] <- bbox_new["ymax"] + (0.05 * y_range)
+
+# data_bivariate <- data_bivariate %>% 
+#   filter(!is.na(bi_class)) %>% 
+#   filter(!grepl("NA", bi_class))
 
 # Set the map
 flood_bivar_map <-
@@ -268,7 +260,10 @@ flood_bivar_map <-
   tm_shape(data_bivariate) +
   tm_polygons(
     fill = "bi_class",
-    fill.scale = tm_scale_categorical(values = custom_pal_red, value.na = "grey"),
+    fill.scale = tm_scale_categorical(
+      values = custom_pal_red, 
+      value.na = "grey80"  # <--- Explicitly define the color for NA values here
+    ),
     fill.legend = tm_legend(show = FALSE),
     col = "black",
     col_alpha = 0.5,
@@ -281,11 +276,12 @@ flood_bivar_map <-
     size = 0.7,
     col = "black",
       bgcol = "white",  
-      bgcol_alpha = 0.7,
+      bgcol_alpha = 0.5,
     options = opt_tm_labels(just = "center") 
     ) +
   
-  tm_title("Flood Exposure vs Vulnerability") + 
+  tm_title("Análisis de Riesgo de Inundaciones - El Salvador",
+           size = 1) + 
   tm_logo("flood_biv_legend.png", height = 5.5, position = c("left", "bottom")) +
   tm_layout(
     frame = FALSE,
@@ -309,7 +305,7 @@ print(flood_bivar_map)
 print(legend)
 
 
-## 3.2 Vulnerability IVMC and Exposure Maps ----
+## 3.2 Vulnerability IVMC + Exposure ----
 
 # We use 'unname' so tmap maps the 1st color to "1", 2nd to "2", etc.
 clean_risk_pal <- unname(c(
@@ -412,14 +408,16 @@ tmap_save(
   dpi = 300        # Resolution (300 is print quality)
 )
 
-## 3.3 Map the 3 Vulnerability Dimensions 
-dist_to_map <- c("D1", "D2", "D3")
+## 3.3 Map the Vulnerability Dimensions ----
+dims_to_map <- c("D1", "D2", "D3", "IVMC")
 
-palettes <- c(c("Purples", "Greens", "Oranges"))
+palettes <- c(c("brewer.purples", "brewer.greens", "brewer.blues", "brewer.oranges"))
 
 titles <- c("Dimensión 1: Vulnerabilidad", 
             "Dimensión 2: Adaptabilidad",
-            "Dimensión 3: Diferencial Demográfico")
+            "Dimensión 3: Diferencial Demográfico",
+            "Indice de Vulnerabilidad Climática y Medioambiental - IVMC")
+
 
 for (i in 1:length(dims_to_map)) {
   
@@ -430,69 +428,320 @@ for (i in 1:length(dims_to_map)) {
   
   print(paste("Mapping:", var_name))
   
-  # Build the Map
+  # Calculate Breaks & Labels Manually ---
+    # Get the data column for this dimension
+    vals <- data_bivariate[[var_name]]
+  
+  # Calculate 3 quantile breaks (33%, 66%)
+  breaks <- quantile(vals, probs = c(0, 1/3, 2/3, 1), na.rm = TRUE)
+  b      <- round(breaks, 3) # Round to 2 decimals
+  
+  # Create clean labels with symbols
+  # 1. Low:  "<= X"
+  # 2. Mid:  "X - Y"
+  # 3. High: "> Y"
+  custom_labels <- c(
+    paste0("< ", b[2]),
+    paste0(b[2], " - ", b[3]),
+    paste0("> ", b[3])
+  )
+  
+  # build the map
   vulnerability_map <- 
-    # --- Context Layer (Sea & Neighbors) ---
-    tm_shape(ab, bbox = bbox_new) + # Uses your expanded bbox
+    # Context
+    tm_shape(ab, bbox = bbox_new) +
     tm_polygons(fill = "#f0f0f0", col = "black", lwd = 0.3) +
     
-    # --- Main Data Layer ---
+    # Main Data
     tm_shape(data_bivariate) +
     tm_polygons(
       fill = var_name,
       
-      # Quantile Scale with Specific Palette
+      # Use FIXED style with our custom logical labels
       fill.scale = tm_scale_intervals(
-        style = "quantile", 
-        n = 5,                 # 5 classes (Quintiles)
-        values = pal_name,     # The unique color ramp
-        value.na = "transparent"
+        style = "fixed", 
+        breaks = breaks,
+        labels = custom_labels, # <--- Insert the custom text here
+        values = pal_name,
+        value.na = "darkgrey"
       ),
       
       col = "black", 
       lwd = 0.1, 
       col_alpha = 0.5,
-      fill.legend = tm_legend(title = "") # Clean legend
+      fill.legend = tm_legend(title = "")
     ) +
     
-    # --- Layout ---
+    # Layout
     tm_title(map_title) +
     tm_layout(
       frame = FALSE, 
       bg.color = "#dbf1ff",
       legend.position = c("left", "bottom")
     )
+  
+  # --- Step C: Save ---
+  tmap_save(
+    tm = vulnerability_map,
+    filename = paste0(dir, "maps/Vulnerability_Map_", var_name, ".jpg"),
+    dpi = 300,
+    width = 10,
+    height = 8
+  )
 }
 
+## 3.4 Map Flood Hazard Zones ----
 
-### 3.3.1 D1 Vulnerabilidad
-map_viln_d1 <-  
-  tm_shape(ab, bbox = data_bivariate) +
+flood_colors <- c("#eff3ff", "#bdd7e7", "#6baed6", "#08519c")
+
+# 2. Create the map
+flood_hazard_map <- 
+  
+  # Background
+  tm_shape(ab, bbox = bbox_new) +
+  tm_polygons(fill = "#f0f0f0", col = "black", lwd = 0.3) +
+  
+  # Map
+  tm_shape(haz_flood) +
   tm_polygons(
-    fill = "#f0f0f0",  
-    col = "black",      
-    lwd = 0.3           
+    fill = "risk_class",
+    col  = "risk_class",   # <--- The Trick: Border matches Fill
+    lwd  = 0.5,            # Small width to bridge the gap
+    
+    # Define the same palette for BOTH fill and col
+    fill.scale = tm_scale_categorical(values = flood_colors),
+    col.scale  = tm_scale_categorical(values = flood_colors),
+    
+    # Hide the extra legend generated by 'col'
+    col.legend  = tm_legend_hide(),
+    fill.legend = tm_legend(title = "Nivel de Amenaza",
+                            position = c("right", "top"))
   ) +
-  tm_shape(data_bivariate) +
-  tm_polygons(
-    fill = "bi_y",
-    fill.scale = tm_scale_categorical(
-      values = clean_vuln_pal,         
-      labels = vuln_labels_text        # <--- Use the text directly
-    ),
-    fill.legend = tm_legend(title = "Dimensión1: Vulnerabilidad"),
-    col = "black", lwd = 0.1
-  ) 
+    # Text below title
+  tm_credits(
+    text = "Categorizado por Intensidad", 
+    size = 0.9,                 # Slightly smaller than title (default is ~1.0)
+    col = "grey30",             # Dark grey for visual hierarchy
+    position = c("left", "top") # Same position as title
+  ) +
+  
+  # Titles and Layout
+  tm_title("Evaluación de Amenaza de Inundación") +
+  tm_layout(
+    frame = FALSE,
+    inner.margins = c(0.05, 0.05, 0.05, 0.05),
+      bg.color = "#dbf1ff"  # Light blue "Sea" color
+    )
+
+# View it
+flood_hazard_map
+
+# Save
+tmap_save(
+  tm = flood_hazard_map,
+  filename = paste0(dir, "maps/Flood_Hazard_SLV.jpg"),
+  dpi = 300,
+  width = 10,
+  height = 8
+)
+
+# Para el IVMC a lo mejor necesito los limites para los tramos de las leyendas y representacion
 
 # 4. WORKING AROUND OUTPUTS ===================================================
 ## 4.1 Export layers to map risk in QGIS ----
+
 writeVector(vect(data_bivariate), paste0(dir,"layers/risk_assessment/slv_risk_assessment_districts.gpkg"),
             layer = 'floods_dist_bivar',
             overwrite = T)
 
+## 4.2 Export Flood table into excel but clean names and fields ----
+names(data_bivariate)
+
+flood_risk_dist_table <- data_bivariate %>% 
+  as_data_frame() %>% 
+  st_drop_geometry() %>% 
+  arrange(NA3) %>% 
+  select(-c(bi_x, bi_y, IVMC_jitter, flood_jitter, geometry)) %>% 
+  rename(
+    `District Code` = NA3,
+    `District Name` = NAM,
+    `Población Wpop 2025` = wpop_risk,
+    `Vulnerabilidad Sensibilidad` = D1,
+    `Vulnerabilidad Adaptabilidad` = D2,
+    `Vulnerabilidad Diferencial Demografico` = D3,
+    `Pobl. Amenaza Extrema Inundación` = flood_extremo,
+    `Pobl. Amenaza Alta Inundación` = flood_alto,
+    `Pobl. Amenaza Media Inundación` = flood_medio,
+    `Pobl. Amenaza Baja Inundación` = flood_bajo,
+    `% Pobl. Amenaza Extrema Inundación` = per_flood_extremo,
+    `% Pobl. Amenaza Alta Inundación` = per_flood_alto,
+    `% Pobl. Amenaza Media Inundación` = per_flood_medio,
+    `% Pobl. Amenaza Baja Inundación` = per_flood_bajo,
+    `% Pobl. Amenaza Extrema + Alta Inundación` = flood_exp_ext_alt,
+    `Clase Riesgo Inundacion - IVMC` = bi_class)
+
+
+# Create a blank Workbook
+wb <- createWorkbook()
+
+# Add a Sheet
+sheet_name <- "Riesgo_Inundaciones_SLV"
+addWorksheet(wb, sheet_name)
+
+# Write the Data
+writeData(wb, sheet_name, flood_risk_dist_table, startRow = 1, startCol = 1)
+
+# Styles for excel table
+
+# Style 1: Header (Blue background, White Bold Text)
+header_style <- createStyle(
+  fontSize = 11, 
+  fontColour = "white", 
+  fgFill = "#08519c", # Blue color from your map
+  halign = "center", 
+  valign = "center",
+  textDecoration = "bold",
+  border = "Bottom"
+)
+
+# Style 2: Percentage Columns 
+# "0.0%" format converts 0.12 to 12.0%
+pct_style <- createStyle(numFmt = "0.0%") 
+
+# Style 3: Population/Number Columns (Comma separator)
+num_style <- createStyle(numFmt = "#,##0")
+
+# Style 4: Population ratios for vulnerability
+rat_style <- createStyle(numFmt = "0.000")
+
+# --- APPLY STYLES ---
+
+# Apply Header Style
+addStyle(wb, sheet_name, header_style, rows = 1, cols = 1:ncol(flood_risk_dist_table), gridExpand = TRUE)
+
+# Apply Percent Style to the specific columns (indices 11 to 14)
+addStyle(wb, sheet_name, pct_style, rows = 2:(nrow(flood_risk_dist_table)+1), cols = 12:16, gridExpand = TRUE)
+
+# Apply Number Style to Population columns (indices 7, 8, 9, 10)
+addStyle(wb, sheet_name, num_style, rows = 2:(nrow(flood_risk_dist_table)+1), cols =  7:11, gridExpand = TRUE)
+
+# Apply Ratio Style to Vulnerability columns (indices ., 4, 5, 6)
+addStyle(wb, sheet_name, rat_style, rows = 2:(nrow(flood_risk_dist_table)+1), cols = 3:6, gridExpand = TRUE)
+
+# Freeze the top row so it stays visible when scrolling
+freezePane(wb, sheet_name, firstRow = TRUE)
+
+# Auto-adjust column widths to fit the text
+setColWidths(wb, sheet_name, cols = 1:ncol(flood_risk_dist_table), widths = "auto")
+
+# Save the file
+saveWorkbook(wb, paste0(dir, "tables/SLV_Risk_Assessment.xlsx"), overwrite = TRUE)
+
+
+## 4.3 Graphics to include in report ----
+
+### 4.3.1 Top 10 Flood Risk % population ----
+# Filter for the top 10 districts by percentage risk
+top_risk <- data_bivariate %>% 
+  filter(bi_class %in% c("3-3","2-3","3-2")) %>% 
+  mutate(flood_exp_ext_alt = flood_exp_ext_alt*100)
+  
+top_10_risk_chart <- ggplot(top_risk, 
+       # Reorder the District Name (Y-axis) based on the risk percentage (X-axis) 
+       # so the highest risk is at the top.
+       aes(x = flood_exp_ext_alt , 
+           y = fct_reorder(NAM, flood_exp_ext_alt))
+       )+
+  
+  # Create the bar geometry
+  geom_col(fill = "#0072B2", width = 0.7) + 
+  
+  # Add risk percentage labels to the end of the bars
+  geom_text(aes(label = paste0(round(flood_exp_ext_alt), "%")), 
+            hjust = -0.1, 
+            size = 3,
+            col = "#333333") +
+  
+  # Set chart title and axis labels
+  labs(
+    title = "% de Población en zona de amenaza extrema y alta de inundaciones \nen los 10 distritos con mayor riesgo",
+    x = "% de población en zonas de amenaza extrema y alta a inundación  (%)",
+    y = "Distrito"
+  ) +
+  
+  # Customize the theme (optional, for aesthetics)
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    axis.title.x = element_text(margin = margin(t = 10)),
+    axis.title.y = element_text(margin = margin(r = 10))
+  ) +
+  
+  # Ensure the X-axis starts at 0 and doesn't exceed 100
+  scale_x_continuous(limits = c(0, max(top_risk$flood_exp_ext_alt) * 1.1),
+                     labels = function(x) paste0(x, "%"))
+
+top_10_risk_chart
+
+ggsave(
+  filename = paste0(dir, "maps/Top10_Risk_Chart.jpg"), # Saving to your existing maps folder
+  plot = top_10_risk_chart,
+  device = "jpeg",
+  width = 8,    # Width in inches
+  height = 6,   # Height in inches
+  dpi = 300,    # High resolution for reports
+  bg = "white"  # CRITICAL: Ensures white background instead of transparent/black
+)
+
+### 4.3.2 Top 10 flood risk districts Population counts ---- 
+top_risk_pop <- top_risk %>% 
+  mutate(pop_risk_ext_alt = flood_extremo   + flood_alto )
+
+top_10_risk_pop_chart <- ggplot(top_risk_pop, 
+                            # Reorder the District Name (Y-axis) based on the risk percentage (X-axis) 
+                            # so the highest risk is at the top.
+                            aes(x = pop_risk_ext_alt , 
+                                y = fct_reorder(NAM, pop_risk_ext_alt))
+)+
+  
+  # Create the bar geometry
+  geom_col(fill = "orange", width = 0.7) + 
+  
+  # Add risk percentage labels to the end of the bars
+  geom_text(aes(label = round(pop_risk_ext_alt,0)), 
+            hjust = -0.1, 
+            size = 3,
+            col = "#333333") +
+  
+  # Set chart title and axis labels
+  labs(
+    title = "Población en zona de amenaza extrema y alta de inundaciones \nen los 10 distritos con mayor riesgo",
+    x = "Población en zonas de amenaza extrema y alta a inundación (hab.)",
+    y = "Distrito"
+  ) +
+  
+  # Customize the theme (optional, for aesthetics)
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    axis.title.x = element_text(margin = margin(t = 10)),
+    axis.title.y = element_text(margin = margin(r = 10))
+  ) 
+
+top_10_risk_pop_chart
+
+# Save
+ggsave(
+  filename = paste0(dir, "maps/Top10_Risk_Chart_pop.jpg"), # Saving to your existing maps folder
+  plot = top_10_risk_pop_chart,
+  device = "jpeg",
+  width = 8,    # Width in inches
+  height = 6,   # Height in inches
+  dpi = 300,    # High resolution for reports
+  bg = "white"  # CRITICAL: Ensures white background instead of transparent/black
+) 
+# Graph 
 # TO DO
-# Mejorar mapa bivariable. Incluir fronteras paises alrededor, bkground azul para visualizar mar
-# anadir etiquetas en distritos alto riesgo
 
 # Sacar tablas de resultados y graficos para exportar en imagen
 
